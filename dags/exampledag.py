ff907c538d0f941e338e5ac784b5ee3751fb885b
jangwonik <dhslrl321@gmail.com>
2024-07-07 00:58:05 +0900

    commit

---
+++ dags/exampledag.py
@@ -0,0 +1,59 @@
+import requests
+from airflow import Dataset
+from airflow.decorators import dag, task
+from pendulum import datetime
+
+### astro project 에 의해 생성된 파일
+@dag(
+    # Define the basic parameters of the DAG, like schedule and start_date
+    start_date=datetime(2024, 1, 1),
+    schedule="@daily",
+    catchup=False,
+    doc_md=__doc__,
+    default_args={"owner": "Astro", "retries": 3},
+    tags=["example"],
+)
+def example_astronauts():
+    # Define tasks
+    @task(
+        # Define a dataset outlet for the task. This can be used to schedule downstream DAGs when this task has run.
+        outlets=[Dataset("current_astronauts")]
+    )  # Define that this task updates the `current_astronauts` Dataset
+    def get_astronauts(**context) -> list[dict]:
+        """
+        This task uses the requests library to retrieve a list of Astronauts
+        currently in space. The results are pushed to XCom with a specific key
+        so they can be used in a downstream pipeline. The task returns a list
+        of Astronauts to be used in the next task.
+        """
+        r = requests.get("http://api.open-notify.org/astros.json")
+        number_of_people_in_space = r.json()["number"]
+        list_of_people_in_space = r.json()["people"]
+
+        context["ti"].xcom_push(
+            key="number_of_people_in_space", value=number_of_people_in_space
+        )
+        return list_of_people_in_space
+
+    @task
+    def print_astronaut_craft(greeting: str, person_in_space: dict) -> None:
+        """
+        This task creates a print statement with the name of an
+        Astronaut in space and the craft they are flying on from
+        the API request results of the previous task, along with a
+        greeting which is hard-coded in this example.
+        """
+        craft = person_in_space["craft"]
+        name = person_in_space["name"]
+
+        print(f"{name} is currently in space flying on the {craft}! {greeting}")
+
+    # Use dynamic task mapping to run the print_astronaut_craft task for each
+    # Astronaut in space
+    print_astronaut_craft.partial(greeting="Hello! :)").expand(
+        person_in_space=get_astronauts()  # Define dependencies using TaskFlow API syntax
+    )
+
+
+# Instantiate the DAG
+example_astronauts()
