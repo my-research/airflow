8cc20a3fabeafecd46da5cdebbf31ba544559d94
jangwonik <dhslrl321@gmail.com>
2024-07-07 18:51:47 +0900

    commit

--- dags/s02_task_direction.py
+++ dags/s02_task_direction.py
@@ -1,16 +1,15 @@
-from airflow import DAG
+from airflow.decorators import dag
+
 from airflow.operators.bash import BashOperator
 from pendulum import datetime
 
-"""
-기본적으로 dags 는 다음과 같이 정의할 수 있음
-"""
-with DAG(
+@dag(
     dag_id='s02_task_direction',
-    schedule="0 0 * * *",
     start_date=datetime(2024, 7, 7, tz="Asia/Seoul"),
+    schedule="0 0 * * *",
     catchup=False,
-) as dag:
+)
+def s02_task_direction():
     t1 = BashOperator(task_id='t1', bash_command='echo hello t1')
     t2 = BashOperator(task_id='t2', bash_command='echo hello t2')
     t3 = BashOperator(task_id='t3', bash_command='echo hello t3')
@@ -20,6 +19,9 @@ with DAG(
     t7 = BashOperator(task_id='t7', bash_command='echo hello t7')
     t8 = BashOperator(task_id='t8', bash_command='echo hello t8')
 
-t1 >> [t2, t3] >> t4
-t5 >> t4
-[t4 >> t7] >> t6 >> t8
+    t1 >> [t2, t3] >> t4
+    t5 >> t4
+    [t4 >> t7] >> t6 >> t8
+
+
+s02_task_direction()
